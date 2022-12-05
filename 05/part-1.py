b25eba177c7a6537382d7eb05714545c566c3ba1
Richard Roberts <richard.andrew.roberts@gmail.com>
2022-12-05 19:07:32 +1300

    Day 05

---
+++ 05/part-1.py
@@ -0,0 +1,44 @@
+# Get the lines
+with open("input.txt", "r") as f:
+    lines = [l.replace("\n", "") for l in f.readlines()]
+
+# Remeber line index
+line_ix = 0
+
+# Parse the stack map
+n = len(lines[0]) // 4 + 1
+cols = [[] for _ in range(n)]
+while "[" in lines[line_ix]:
+    line = f"{lines[line_ix]}   "
+    for i in range(n):
+        if line[i*4:i*4+4] == "    ":
+            # cols[i].insert(0, "x")
+            pass
+        else:
+            cols[i].insert(0, line[i*4+1])
+    line_ix += 1
+
+# Skip the column labels line
+line_ix += 1 
+
+# Skip the empty line
+line_ix += 1
+
+def log():
+    print("-" * 40)
+    for (ix, c) in enumerate(cols):
+        print(f"{ix}: {'|'.join(c)}")
+    print("-" * 40)
+
+# Process moves
+log()
+while line_ix < len(lines):
+    line = lines[line_ix]
+    parts = line.split(" ")
+    n = int(parts[1])
+    i_from = int(parts[3]) - 1
+    i_to = int(parts[5]) - 1
+    for _ in range(n): cols[i_to].append(cols[i_from].pop())
+    line_ix += 1
+log()
+print("".join([c[-1] for c in cols]))
